6d53decbf5bbdddf8eb13502d85d809acb316892
John Enad <jenad88@hotmail.com>
2023-07-27 09:07:19 -0400

    add various components for sample

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,7 @@
+export default function HomePage() {
+  return (
+    <div className='text-center p-5 text-xl'>
+      <h1 className='text-xl text-slate-900'>Welcome to Home Page!</h1>
+    </div>
+  )
+}
