3f304a0b7a5c48b9259920864b6d90222aa0503a
John Enad <jenad88@hotmail.com>
2023-07-29 22:23:51 -0400

    update testing mocks

--- src/testing/mocks/initialize.ts
+++ src/testing/mocks/initialize.ts
@@ -1,13 +1,19 @@
 import { IS_SERVER } from '../../config/constants';
+import { seedDb } from './seed-db';
 
 const initializeMocks = () => {
+  console.log('initializeMocks')
   if (IS_SERVER) {
+    console.log('initializeMocks IS_SERVER')
     const { server } = require('./server');
     server.listen();
   } else {
+    console.log('initializeMocks IS_BROWSER')
     const { worker } = require('./browser');
     worker.start();
   }
+  console.log('initializeMocks seedDb')
+  seedDb();
 };
 
 initializeMocks();
